e3744de17606a2db894e3b67a747e7f9320905f2
chani-klein <chani4166742@gmail.com>
2025-05-05 18:10:27 +0300

    6.5.2025

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
-import { Provider } from "react-redux";
-import store from "./redux/store";
-import Router from "./routes/Router";
-import InitializedAuth from "./auth/InitializedAuth";
+// import { Provider } from "react-redux";
+// import store from "./redux/store";
+// import Router from "./routes/Router";
+// import InitializedAuth from "./auth/InitializedAuth";
+
+// function App() {
+//   return (
+//     <Provider store={store}>
+//       <InitializedAuth />
+//       <Router />
+//     </Provider>
+//   );
+// }
+
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppRoutes from './routes/AppRoutes';
 
 function App() {
   return (
-    <Provider store={store}>
-      <InitializedAuth />
-      <Router />
-    </Provider>
+    <Router>
+    <AppRoutes />
+    </Router>
   );
 }
 
-
 export default App;
+
+
---
+++ src/pages/Home.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Home.css';
+
+const Home = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="home-container">
+      <div className="overlay">
+        <h1 className="main-title">
+          הורידו כעת את האפליקציה<br />
+          הירשמו עכשיו כדי שתוכלו<br />
+          בזמן אמת להציל חיים
+        </h1>
+        <div className="button-group">
+          <button className="btn-register" onClick={() => navigate('/register-user')}>
+            להרשמה
+          </button>
+          <button className="btn-volunteer" onClick={() => navigate('/register-volunteer')}>
+            הרשם <br /> כמתנדב
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
