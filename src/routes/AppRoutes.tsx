9509921f5212241c94bef17eec6bb82d567e11e6
chani-klein <chani4166742@gmail.com>
2025-05-05 18:34:15 +0300

    chani

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
-import RegisterUser from '../pages/RegisterUser';
-import RegisterVolunteer from '../pages/RegisterVolunteer';
+import { LoginPage } from '../pages/LoginPage';
+// import RegisterVolunteer from '../pages/RegisterVolunteer';
+
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/register-user" element={<RegisterUser />} />
-      <Route path="/register-volunteer" element={<RegisterVolunteer />} />
+      <Route path="/register-user" element={<LoginPage />} />
+      {/* <Route path="/register-volunteer" element={<RegisterVolunteer />} /> */}
+
+
     </Routes>
   );
 };
