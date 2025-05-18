c7768a83eef1bc1fec24a31d6325294d06096c1e
RIVKI <rivkiasher@gmail.com>
2025-05-19 01:20:36 +0300

    add

--- src/pages/EmergencyPage.tsx
+++ src/pages/EmergencyPage.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import BackgroundLayout from "../layouts/BackgroundLayout";
-import "../layouts/BackgroundLayout.css" // אם יש לך עיצוב לכפתור
+import '../la'
 
 export default function EmergencyPage() {
   const navigate = useNavigate();
